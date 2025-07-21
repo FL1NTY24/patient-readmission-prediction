66341f2d6ba5f828a8addcec607031b3c39f56cf
FL1NTY24 <107490001+FL1NTY24@users.noreply.github.com>
2025-07-21 12:58:40 +0200

    Update provider.tf

--- infrastructure/provider.tf
+++ infrastructure/provider.tf
@@ -1,15 +1,17 @@
-provider "aws" {
-       region = "us-east-1"
-       endpoints {
-         s3 = var.localstack_enabled ? "http://localhost:4566" : null
-       }
-       skip_credentials_validation = var.localstack_enabled
-       skip_requesting_account_id  = var.localstack_enabled
-       skip_metadata_api_check     = var.localstack_enabled
-     }
+variable "localstack_enabled" {
+  type    = bool
+  default = false
+}
 
-     variable "localstack_enabled" {
-       description = "Whether to use LocalStack for local testing"
-       type        = bool
-       default     = true
-     }
+provider "aws" {
+  region                      = "us-east-1"
+  access_key                  = var.localstack_enabled ? "test" : null
+  secret_key                  = var.localstack_enabled ? "test" : null
+  skip_credentials_validation = var.localstack_enabled
+  skip_requesting_account_id  = var.localstack_enabled
+  skip_metadata_api_check     = var.localstack_enabled
+  endpoints {
+    s3  = var.localstack_enabled ? "http://localhost:4566" : null
+    sns = var.localstack_enabled ? "http://localhost:4566" : null
+  }
+}
