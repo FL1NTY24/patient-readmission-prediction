6f6755ff42d8cd31fc57743745c5a26c98080a6c
FL1NTY24 <107490001+FL1NTY24@users.noreply.github.com>
2025-07-21 11:25:53 +0200

    Update variables.tf

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,5 +1,17 @@
 variable "bucket_name" {
-       description = "Name of the S3 bucket"
-       type        = string
-       default     = "readmission-bucket"
-     }
+  description = "Name of the S3 bucket"
+  type        = string
+  default     = "readmission-bucket"
+}
+
+variable "ecs_cluster_name" {
+  description = "Name of the ECS cluster"
+  type        = string
+  default     = "readmission-api-cluster"
+}
+
+variable "sns_topic_name" {
+  description = "Name of the SNS topic"
+  type        = string
+  default     = "readmission-monitoring-alerts"
+}
