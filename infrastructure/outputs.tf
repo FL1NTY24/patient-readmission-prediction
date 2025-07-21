60eba2e9b981d0aa034168194952317a492ded69
FL1NTY24 <107490001+FL1NTY24@users.noreply.github.com>
2025-07-21 11:26:11 +0200

    Update outputs.tf

--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -1,3 +1,11 @@
 output "s3_bucket_name" {
-       value = aws_s3_bucket.readmission_bucket.bucket
-     }
+  value = aws_s3_bucket.readmission_bucket.bucket
+}
+
+output "ecs_cluster_name" {
+  value = aws_ecs_cluster.api_cluster.name
+}
+
+output "sns_topic_arn" {
+  value = aws_sns_topic.monitoring_alerts.arn
+}
