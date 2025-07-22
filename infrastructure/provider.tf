6132f2bc2b1dd820b41386d548bea0838710fa93
FL1NTY24 <107490001+FL1NTY24@users.noreply.github.com>
2025-07-22 11:31:07 +0200

    Update provider.tf

--- infrastructure/provider.tf
+++ infrastructure/provider.tf
@@ -5,6 +5,7 @@ provider "aws" {
   skip_credentials_validation = var.localstack_enabled
   skip_requesting_account_id  = var.localstack_enabled
   skip_metadata_api_check     = var.localstack_enabled
+  s3_force_path_style         = var.localstack_enabled ? true : false
   endpoints {
     s3  = var.localstack_enabled ? "http://localhost:4566" : null
     sns = var.localstack_enabled ? "http://localhost:4566" : null
