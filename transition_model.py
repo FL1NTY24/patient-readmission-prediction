e0507b66317d5d063542b1e6b8d155eca9328c32
FL1NTY24 <107490001+FL1NTY24@users.noreply.github.com>
2025-07-28 12:23:17 +0200

    Add files via upload

---
+++ transition_model.py
@@ -0,0 +1,8 @@
+from mlflow.tracking import MlflowClient
+
+client = MlflowClient(tracking_uri="http://127.0.0.1:5000")
+client.transition_model_version_stage(
+    name="ReadmissionModel",
+    version=18,  # Replace with the correct version number from the UI
+    stage="Production"
+)
